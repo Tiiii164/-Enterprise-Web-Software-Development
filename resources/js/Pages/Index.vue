485dc93890adf4fe1b43202a684fe6009b70f4fe
Koshice <khoa93.pch@gmail.com>
2023-02-07 17:03:44 +0700

    laravel push

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <the-header></the-header>
+    </div>
+
+</template>
+
+<script>
+import TheHeader from '../components/TheHeader.vue';
+
+export default {
+    name: "Index",
+    components: {
+        TheHeader,
+    },
+}
+</script>
---
+++ resources/js/components/TheHeader.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>ABCXYZ</h1>
+</template>
