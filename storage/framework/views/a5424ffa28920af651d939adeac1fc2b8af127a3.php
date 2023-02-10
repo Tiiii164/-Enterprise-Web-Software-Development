ee08a6c9e8ece228a0e88581c0b1f2d84186f45a
nguyen <pnu2410@gmail.com>
2023-02-10 20:40:35 +0700

    của ai qtroi nè

---
+++ storage/framework/views/a5424ffa28920af651d939adeac1fc2b8af127a3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia><?php echo e(config('app.name', 'Laravel')); ?></title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Scripts -->
+        <?php echo app('Tightenco\Ziggy\BladeRouteGenerator')->generate(); ?>
+        <?php echo app('Illuminate\Foundation\Vite')(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"]); ?>
+        <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->head; } ?>
+    </head>
+    <body class="font-sans antialiased">
+        <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->body; } else { ?><div id="app" data-page="<?php echo e(json_encode($page)); ?>"></div><?php } ?>
+    </body>
+</html>
+<?php /**PATH C:\Users\pnu24\-Enterprise-Web-Software-Development\resources\views/app.blade.php ENDPATH**/ ?>
