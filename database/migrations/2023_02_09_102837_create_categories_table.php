57696ad26ecb81bff7218aec1960da6bae791073
Koshice <khoa93.pch@gmail.com>
2023-02-18 23:59:14 +0700

    Update 11h59

--- database/migrations/2023_02_09_102837_create_categories_table.php
+++ database/migrations/2023_02_09_102837_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name');
             $table->dateTime('day_time');
         });
