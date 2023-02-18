724541877a50db43c2aaa96140e04afcb75554e9
phucthien432002 <106296783+tuankietha@users.noreply.github.com>
2023-02-18 16:34:02 +0700

    create footer

---
+++ resources/js/Components/TheFooter.vue
@@ -0,0 +1,64 @@
+<template>
+    <section class="box">
+        <footer class="footer1">
+            <div class="img1">
+                <a href=""><img src="../../css/facebook.png" width="30" height="30"></a>
+            </div>
+            <div class="img1">
+                <a href=""><img src="../../css/instagram.png" width="30" height="30"></a>
+            </div>
+            <div class="img1">
+                <a href=""><img src="../../css/zalo.png" width="30" height="30"></a>
+            </div>
+            <div class="img1">
+                <a href=""><img src="../../css/youtube.png" width="30" height="30"></a>
+            </div>
+        </footer>
+        <footer class="footer2">
+            
+             <div class="text2">
+                <h6>Privacy Policy</h6>
+             </div>
+             <div class="text2">
+                <h6>Terms Of Service</h6>
+             </div>
+             <div class="text2">
+                <h6>Help Center</h6>
+             </div>
+             <div class="img2">
+                <img src="../../css/copyright.png" width="20" height="20">6 AE Siêu Nhân 2023
+             </div>
+             
+        </footer>
+       
+    </section>
+</template>
+<style>
+.box{
+    width: 100%;
+    height: 150px;
+    background-color: #DCDCDC;
+}
+.footer1{
+    display: flex;
+    align-items: center;
+   justify-content: center;
+}
+.footer1 .img1{
+    padding: 20px;
+}
+.footer2{
+    display: flex;
+    justify-content: center;
+    width: 100%;
+}
+.footer2 .text2{
+    margin-top: 4px;
+    margin-left: 12px;
+    font-family: cursive;
+    padding-right: 20px;
+}
+.footer2 .img2{
+    font-family: cursive;
+}
+</style>
