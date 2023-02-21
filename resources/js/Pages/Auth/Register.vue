f0bfb14810247dc34d66cb196856970377f81d03
phucthien432002 <106296783+tuankietha@users.noreply.github.com>
2023-02-21 21:28:07 +0700

    login&register

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -35,38 +35,42 @@ import { useRouter } from "vue-router"
 }
 </script>
 <template>
-    <div class="container position-absolute top-50 start-50 translate-middle">
-        <div class="row">
-            <div class="col-md-8 m-auto">
-                <div class="card">
+    <div class="kimetsu">
+        <div class="row1">
+            <div class="col-md-8 ">
+                <div class="">
                 <div class="card-body">
-                    <h4 class="card-title">Register Form</h4>
+                    <h1 class="card-title">START FOR FREE</h1>
+                    <h1 class="card-title1">Create new account.</h1>
                     <p class="list-disc text-red-400" v-if="typeof errors === 'string'">{{errors}}</p>
                     <form method="post" @submit.prevent="handleRegister">
                         <div class="mb-3">
                             <label for="name" class="form-label">Name: </label>
-                            <input type="text" name="name" v-model="form.name" id="username" class="form-control" placeholder="Enter Name">
+                            <input type="text" name="name" v-model="form.name" id="username"  class="border" placeholder=" Enter Name" plain>
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email: </label>
-                            <input type="text" name="email" v-model="form.email" id="email" class="form-control" placeholder="Enter Email">
+                            <input type="text" name="email" v-model="form.email"  id="email" class="border" placeholder=" Enter Email" plain>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password: </label>
-                            <input type="password" name="password" v-model="form.password" id="password" class="form-control" placeholder="Enter Password">
+                            <input type="password" name="password" v-model="form.password" id="password" class="border" placeholder=" Enter Password" plain>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Confirm Password: </label>
-                            <input type="password" name="password_confirm" v-model="form.password_confirmation" id="password_confirm" class="form-control" placeholder="Confirm Password">
+                            <input type="password" name="password_confirm" v-model="form.password_confirmation" class="border" id="password_confirm"  placeholder=" Confirm Password">
                         </div>
-                        <div class="d-flex justify-content-between">
-                            <router-link class="btn btn-danger" to="/Login"> Login </router-link>
-                            <button type="submit" class="btn btn-primary">Register</button>
+                        <div class="d-flex justify-center">
+                            <router-link class="buttoncss ml-16" to="/Login"> Login </router-link>
+                            <button type="submit" class="buttoncss ml-16"> Register </button>
                         </div>
                     </form>      
                 </div>                
                 </div>
             </div>
+        </div>
+        <div>
+            <img src="../../../css/kimetsu.png" alt="" class="kimetsu-img">
         </div>
     </div>
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,13 @@ import './bootstrap';
 import { createApp } from 'vue';
 import router from '../js/Router/router'
 import App from './Layouts/App.vue'
+import { createVuetify } from 'vuetify/lib/framework.mjs';
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
+const vuetify = createVuetify()
 createApp(App)
         .use(router)
+        .use(vuetify)
         .mount('#app');
