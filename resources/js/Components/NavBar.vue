28d7a9e9834451cac2e415df2db96232224fac47
phucthien432002 <106296783+tuankietha@users.noreply.github.com>
2023-03-12 15:01:17 +0700

    css

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -63,10 +63,10 @@ export default {
               <router-link class="nav-link active nav-item1" to="/DepartmentsIndex">Department</router-link>
             </li>
             <li class="dropdown-item">
-              <router-link class="nav-link active" to="/RolesIndex">Role</router-link>
+              <router-link class="nav-link active nav-item1" to="/RolesIndex">Role</router-link>
             </li>
             <li class="dropdown-item">
-              <router-link class="nav-link active" to="/UsersIndex">User</router-link>
+              <router-link class="nav-link active nav-item1" to="/UsersIndex">User</router-link>
             </li>
           </ul>
         </li>
