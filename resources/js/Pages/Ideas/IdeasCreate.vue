d2c0df8d311c2c4410e9e0644c188cc039d79269
phucthien432002 <106296783+tuankietha@users.noreply.github.com>
2023-03-12 13:49:27 +0700

    checkbox

--- resources/js/Pages/Ideas/IdeasCreate.vue
+++ resources/js/Pages/Ideas/IdeasCreate.vue
@@ -16,6 +16,10 @@ export default {
       user: [],
       topics: [],
       categories: [],
+      form: {
+        termsAndConditions: false
+      },
+      showDialog: false,
     }
   },
   setup() {
@@ -55,7 +59,13 @@ export default {
         .then((response) => {
           this.topics = response.data;
         });
-    }
+    },
+    showTermsAndConditions() {
+      this.showDialog = true;
+    },
+    hideTermsAndConditions() {
+      this.showDialog = false;
+    },
   },
   created() {
     this.getCategories();
@@ -103,11 +113,20 @@ export default {
                 </select>
               </div>
               <div>
-                <!-- Your form elements here -->
+                <div>
                 <label>
-                  <input type="checkbox" v-model="form.termsAndConditions">
-                  I agree to the terms and conditions
+                  <input type="checkbox" v-model="form.termsAndConditions" @click.stop>
                 </label>
+                <span style="margin-left:5px">I agree to the </span>
+                <span style="text-decoration:underline;cursor:pointer;" @click="showTermsAndConditions">terms and conditions</span>
+              </div>
+                <div v-if="showDialog">
+                  <!-- Your dialog content here -->
+                  <div class="card" style="align-items:center">
+                    <h1>term and condition</h1>
+                    <button class="btn btn-primary" style="width:20%" @click="hideTermsAndConditions">Close</button>
+                  </div>
+                </div>
               </div>
               <button type="submit" class="btn btn-primary mt-2" :disabled="!form.termsAndConditions" @click.prevent="handleCreateIdeas()">Create</button>
             </div>
