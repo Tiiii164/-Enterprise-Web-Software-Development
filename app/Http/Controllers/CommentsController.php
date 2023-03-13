432dce3ee0616985f2ee312c3d21fdf2d2d2231e
Binh Pham <binhptgcs200187@fpt.edu.vn>
2023-03-13 22:03:47 +0700

    fix bug comments

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -4,19 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\models\Comments;
-
+use App\models\Ideas;
 
 use Illuminate\Support\Facades\Auth;
 
 class CommentsController extends Controller
 {
-    public function store(Request $request, Comments $comments)
+    public function store(Request $request, Comments $comments, $ideas_id)
     {
         $comments = new Comments();
         $comments->text = $request->input('text');
         $comments->users_id = Auth::user()->id;
+        $comments->ideas_id = $ideas_id;
+        //$comments->ideas_id = Comments::where('users_id', Auth::user()->id)->value('ideas_id');
         //$comments->ideas_id = Comments::where('ideas_id');
-        $comments->ideas_id = Comments::where('users_id', Auth::user()->id)->value('ideas_id');
         //$comments->ideas_id = Comments::ideas()->id;
         //$comments->ideas_id = request()->route('ideas_id');
         // if (isset($request->ideas_id)) {
