5434c345f7467d9bfa139e15b3a27111795756c4
Koshice <khoa93.pch@gmail.com>
2023-03-25 15:31:33 +0700

    QA Manager

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,6 +9,10 @@ class Role extends LaratrustRole
     public $guarded = [];
     public function users()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class, 'role_user');
+    }
+    public function permissions() 
+    {
+        return $this->belongsToMany(Permission::class,'permission_role');
     }
 }
