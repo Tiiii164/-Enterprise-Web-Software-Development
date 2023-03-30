e5d3ada50ca771183e9a0a887d1fecbeaa1d9248
Koshice <khoa93.pch@gmail.com>
2023-03-31 00:31:20 +0700

    fix 30/3-1

--- app/Http/Controllers/IdeasController.php
+++ app/Http/Controllers/IdeasController.php
@@ -81,6 +81,8 @@ class IdeasController extends Controller
             $file->storeAs($destination_path, $file_name);
             $file_path = $destination_path . "/" . $file_name;
             $ideas->file_path = $file_path;
+        } else {
+            $ideas->file_path = $request->input('file_path');
         }
         $ideas->topics_id = $topics_id;
         $ideas->user_id = Auth::user()->id;
