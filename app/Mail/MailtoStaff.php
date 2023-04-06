2d55bb3aa7ba56060fd02022c4aa3338b0a48571
Koshice <khoa93.pch@gmail.com>
2023-04-06 11:20:25 +0700

    Mail to Staff when create idea

---
+++ app/Mail/MailtoStaff.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Ideas;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+
+class MailtoStaff extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The user instance.
+     *
+     * @var User
+     */
+    public $user;
+
+    /**
+     * The idea instance.
+     *
+     * @var Ideas
+     */
+    public $idea;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, Ideas $idea)
+    {
+        $this->user = $user;
+        $this->idea = $idea;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('systemmanager2002@gmail.com')
+            ->subject('New idea has been created')
+            ->view('emails.notify-idea');
+    }
+}
---
+++ resources/views/emails/notify-idea.blade.php
@@ -0,0 +1,14 @@
+<p>Dear {{ $user-> name }},</p>
+
+<p>A new idea has been created with Email: {{ $user->email }}</p>
+
+<p>Create at: {{ $idea->created_at }} </p>
+
+<p>Title: {{ $idea->title }}</p>
+
+<p>Text: {{ $idea->text }}</p>
+
+<p>
+    Thanks,
+    6 Anh Em Siu Nhan
+</p>
